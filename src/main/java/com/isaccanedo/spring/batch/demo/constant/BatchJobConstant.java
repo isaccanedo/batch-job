50218ed6481a7d9c846dd6460dff4861743e5ccd
isaccanedo <isaccanedo@gmail.com>
2021-06-28 11:10:25 -0300

    :sparkles: Initial commit

---
+++ src/main/java/com/isaccanedo/spring/batch/demo/constant/BatchJobConstant.java
@@ -0,0 +1,12 @@
+package com.isaccanedo.spring.batch.demo.constant;
+
+/**
+ * @author Isac Canedo
+ */
+public interface BatchJobConstant {
+
+    String ORDER_ITEM_READER="orderItemReader";
+    String ORDER_PROCESS_JOB="orderProcessJob";
+
+    String BATCH_STEP="step";
+}
---
+++ src/main/java/com/isaccanedo/spring/batch/demo/entity/Order.java
@@ -0,0 +1,33 @@
+package com.isaccanedo.spring.batch.demo.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+/**
+ * @author Isac Canedo
+ */
+@Data
+@Entity
+@Table(catalog = "batch_demo_db", name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String orderRef;
+
+    private BigDecimal amount;
+
+    private LocalDateTime orderDate;
+
+    private String note;
+
+    @Transient
+    private Date tempOrderDate;
+
+}
